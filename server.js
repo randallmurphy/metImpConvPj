fd33ea2e468198f87ba2f74cb7aa2814d3f9bec3
randall murphy <rmurphy@perseverenow.org>
2025-04-30 10:47:51 -0500

    come back later

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var bodyParser = require('body-parser');
 var expect = require('chai').expect;
 var cors = require('cors');
 const helmet = require('helmet');
-
+var dotenv = require('dotenv')
 var apiRoutes = require('./routes/api.js');
 var fccTestingRoutes = require('./routes/fcctesting.js');
 var runner = require('./test-runner');
